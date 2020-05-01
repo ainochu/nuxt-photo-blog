041085b8af13f150c65067e40761ececb3feff93
Ainoa Andres burguete <ainoaandresburguete@air-de-ainoa.lan>
2020-05-01 22:30:32 +0200

    Resolved issue with alias

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,15 @@
+<template>
+  <div>
+      {{blogPosts}}
+  </div>
+</template>
+
 <script>
 export default {
   computed: {
     blogPosts() {
       return this.$store.state.blogPosts;
-    },
-  },
+    }
+  }
 };
 </script>
