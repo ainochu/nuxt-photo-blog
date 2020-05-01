a8d3657f2b300fd5344333dc4501ffd3d0282255
Ainoa Andres burguete <ainoaandresburguete@air-de-ainoa.lan>
2020-05-01 23:26:52 +0200

    Getting data from pixr to load photos

--- services/data.js
+++ services/data.js
@@ -2,51 +2,51 @@ export const items = [
   {
     id: '1',
     href:
-      "https://geo.itunes.apple.com/us/album/blonde/id1146195596?at=1l3vqFJ&mt=1&app=music",
+      "https://www.flickr.com/photos/188233099@N02/49843106226/",
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/04/23/17/03/17-03-53-549_1280.jpg",
+      "https://live.staticflickr.com/65535/49843106226_0d426728a1_k.jpg",
     imgAlt: "Frank Ocean Blonde Album Cover",
-    title: "Blonde",
-    artist: "Frank Ocean"
+    title: "Pinquito#1",
+    artist: "aandresbur"
   },
   {
     id: '2',
     href:
       "https://geo.itunes.apple.com/us/album/jeffery/id1146718343?at=1l3vqFJ&mt=1&app=music",
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/04/23/17/03/17-03-16-870_1280.jpg",
+      "https://live.staticflickr.com/65535/49843407617_7fe0609423_k.jpg",
     imgAlt: "Young Thug - Jeffery Album Cover",
-    title: "Jeffery",
-    artist: "Young Thug"
+    title: "Pinquito#2",
+    artist: "aandresbur"
   },
   {
     id: '3',
     href:
       "https://geo.itunes.apple.com/us/album/untitled-unmastered./id1089846273?at=1l3vqFJ&mt=1&app=music",
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/04/23/17/03/17-03-35-422_1280.jpg",
+      "https://live.staticflickr.com/65535/49843106511_7cac203791_k.jpg",
     imgAlt: "Kendrick Lamar untitled unmastered. Album Cover",
-    title: "untitled umastered.",
-    artist: "Kendrick Lamar"
+    title: "Pinquito#3",
+    artist: "aandresbur"
   },
   {
     id: '4',
     href:
       "https://geo.itunes.apple.com/us/album/99.9/id1092026376?at=1l3vqFJ&mt=1&app=music",
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/04/23/17/03/17-03-27-208_1280.jpg",
+      "https://live.staticflickr.com/65535/49842563433_81333a8fe5_k.jpg",
     imgAlt: "Kaytranada 99%",
-    title: "99%",
-    artist: "Kaytranada"
+    title: "Pinquito#4",
+    artist: "aandresbur"
   },
   {
     id: '5',
     href:
       "https://geo.itunes.apple.com/us/album/aa/id1067924003?at=1l3vqFJ&mt=1&app=music",
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/04/23/17/03/17-03-43-498_1280.jpg",
+      "https://live.staticflickr.com/65535/49843408142_7f2e004ed2_k.jpg",
     imgAlt: "Baauer Aa - Album Cover",
-    title: "Aa",
-    artist: "Baauer"
+    title: "Pinquito#5",
+    artist: "aandresbur"
   }
 ];
