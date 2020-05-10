e8124ff9fa3fed13605dd52cec61f95790afcfa0
Ainoa Andres burguete <ainoaandresburguete@air-de-ainoa.lan>
2020-05-10 18:49:32 +0200

    Refactor nav-bar and titles

--- components/card.vue
+++ components/card.vue
@@ -1,15 +1,9 @@
 <template>
-  <div class="fl w-60 w-35-m w-30-l pa2">
-    <div class="dim tc">
-      <img :src="imgSrc" :alt="imgAlt" class="w-100 db outline black-10" />
-      <dl class="mt2 f6 lh-copy">
-        <dt class="clip">Title</dt>
-        <dd class="ml0 black truncate w-100">{{title}}</dd>
-        <dt class="clip">Artist</dt>
-        <dd class="ml0 gray truncate w-100">{{artist}}</dd>
-      </dl>
-    </div>
-  </div>
+  <div class="dim">      
+    <img :src="imgSrc" :alt="imgAlt" class="w-100 db tc outline black-10" />
+    <p class="f7 tl ttu pt3 mb0 tracked-mega mt0 code lh-copy measure gray">NOV 5, 2019</p>
+    <p class="f7 tl ttu tracked-mega mt0 code lh-copy measure black">{{artist}}</p>
+  </div>                    
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,33 @@
 <template>
   <article>
-    <header class="bg-white black-80 tc pv4 avenir">
-      <h1 class="mt2 mb0 baskerville i fw1 f1">El rinconciti de Ainowis</h1>
-      <h2 class="mt2 mb0 f6 fw4 ttu tracked">Fotos chupi postu guapis</h2>
-      <nav class="bt bb tc mw7 center mt4">
-        <a class="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" href="/">Home</a>
-        <a class="f6 f5-l link bg-animate black-80 dib pa3 ph4-l" href="/about">About me</a>
+    <header class="nav-bar">
+      <nav class="db dt-l w-100 border-box pa3 ph5-l">
+        <a
+          class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l"
+          href="#"
+          title="Home"
+        >
+          <img
+            src="https://live.staticflickr.com/65535/49867578281_87a924464b.jpg"
+            class="dib w-50 h100 br-100"
+          />
+        </a>
+        <div class="db dtc-l v-mid w-100 w-75-l tc tr-l ttu code lh-copy measure b">
+          <a class="link dim dark-gray f6 pl3 ttu tracked-mega mt0" href="#" title="Home">Home</a>
+          <a class="link dim dark-gray f6 pl3 ttu tracked-mega mt0" href="#" title="Blog">About me</a>
+          <a class="link dim dark-gray f6 pl3 ttu tracked-mega mt0" href="#" title="Press">Blog</a>
+          <a class="link dim dark-gray f6 pl3 ttu tracked-mega mt0" href="#" title="Contact">Contact</a>
+        </div>
       </nav>
     </header>
-    <div class="cf pa8 pl6">
-      <nuxt-link :to="`/albums/${item.id}`" v-for="item in items" :key="item.id" class="db">
+    <p class="pt5 tc f3 ttu tracked-mega mt0">LATEST POSTS</p>
+    <div class="flex flex-wrap cf pa3 card-full">
+      <nuxt-link
+        :to="`/albums/${item.id}`"
+        v-for="item in items"
+        :key="item.id"
+        class="link w-60 w-35-m w-30-l pa2 db"
+      >
         <album-card
           :imgSrc="item.imgSrc"
           :imgAlt="item.imgAlt"
